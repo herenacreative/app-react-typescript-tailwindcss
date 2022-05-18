425fe61a39be5c486b9c6350c351c14c67792b6c
herenacreative <sintaherena@gmail.com>
2022-05-18 11:57:02 +0700

    create app with typescript and tailwind css

---
+++ src/Components/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../index.css'
+
+export default class index extends Component {
+  render() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
+  }
+}
